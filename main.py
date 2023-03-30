6d1185c9a6bc3bcda37d9da7a3e54ed5f6f6c7fc
Rian Kiyas <99842848+Rian-churr@users.noreply.github.com>
2023-03-30 00:03:14 +0000

    changed options to bullets

--- main.py
+++ main.py
@@ -1,85 +1,74 @@
 import tkinter as tk
 
-# Define the questions and their respective answers
-questions = {
-    "What is the capital of France?": ["Paris", "London", "Madrid", "Berlin"],
-    "What is the currency of Japan?": ["Yen", "Dollar", "Euro", "Pound"],
-    "What is the largest country in the world by area?": ["Russia", "Canada", "China", "USA"]
-}
+# Define the questions and their corresponding answers
+questions = [
+    {
+        "question": "What is the capital city of Japan?",
+        "options": ["Tokyo", "Kyoto", "Osaka", "Hiroshima"],
+        "answer": "Tokyo"
+    },
+    {
+        "question": "What is the highest mountain in the world?",
+        "options": ["Mount Kilimanjaro", "Mount Everest", "Mount Fuji", "Mount Denali"],
+        "answer": "Mount Everest"
+    },
+    {
+        "question": "What is the largest country in the world by land area?",
+        "options": ["Russia", "Canada", "China", "USA"],
+        "answer": "Russia"
+    }
+]
 
-# Define the correct answers for each question
-answers = {
-    "What is the capital of France?": "Paris",
-    "What is the currency of Japan?": "Yen",
-    "What is the largest country in the world by area?": "Russia"
-}
+# Define the initial question index
+current_question = 0
 
-# Create the main window for the quiz
-root = tk.Tk()
-root.geometry("400x400")
-
-# Create a label for the quiz title
-title = tk.Label(root, text="World Quiz", font=("Arial", 24))
-title.pack(pady=10)
-
-# Create a label for the question and a variable to store the answer
-question_label = tk.Label(root, font=("Arial", 16))
-question_var = tk.StringVar()
-
-# Create a list of answer buttons
-answer_buttons = []
-
-# Define a function to display the next question
-def next_question():
-    # Clear the previous question and answer buttons
-    question_label.pack_forget()
-    for button in answer_buttons:
-        button.pack_forget()
-    answer_buttons.clear()
-
-    # Get the next question and its answer options
-    question, options = next(iter(questions.items()))
-    question_var.set(question)
-    question_label.pack()
-
-    # Create a button for each answer option
-    for option in options:
-        button = tk.Button(root, text=option, width=10, font=("Arial", 12),
-                           command=lambda o=option: check_answer(o))
-        answer_buttons.append(button)
-        button.pack(pady=2)
-
-    # Create a "Next" button to move to the next question
-    next_button = tk.Button(root, text="Next", width=10, font=("Arial", 12),
-                            command=next_question)
-    answer_buttons.append(next_button)
-    next_button.pack(pady=2)
-
-# Define a function to check the selected answer
-def check_answer(answer):
-    # Get the current question and its correct answer
-    question = question_var.get()
-    correct_answer = answers[question]
-
-    # Check if the selected answer is correct
-    if answer == correct_answer:
-        result_label.config(text="Correct!")
+# Define a function to check the answer
+def check_answer():
+    global current_question
+    selected_option = var.get()
+    if selected_option == "":
+        return
+    if selected_option == questions[current_question]["answer"]:
+        print("Correct!")
     else:
-        result_label.config(text="Incorrect!")
+        print("Incorrect!")
+    current_question += 1
+    if current_question < len(questions):
+        ask_question()
+    else:
+        print("Quiz completed!")
 
-# Create a label for the quiz result
-result_label = tk.Label(root, font=("Arial", 16), fg="green")
-result_label.pack(pady=10)
+# Define a function to ask the current question
+def ask_question():
+    global current_question, var, radio_buttons, next_button
+    question = questions[current_question]
+    question_label.config(text=question["question"])
+    var.set("")
+    for i, option in enumerate(question["options"]):
+        radio_buttons[i].config(text=option, value=option)
+    next_button.config(state="disabled")
+    
+# Define the main window
+root = tk.Tk()
+root.title("World Quiz")
 
-# Create a button to start the quiz
-start_button = tk.Button(root, text="Start Quiz", width=20, font=("Arial", 12),
-                         command=next_question)
-start_button.pack(pady=20)
+# Define the widgets
+question_label = tk.Label(root, text="", font=("Arial", 14))
+var = tk.StringVar(value="")
+radio_buttons = []
+for i in range(4):
+    radio_button = tk.Radiobutton(root, text="", variable=var, value="", font=("Arial", 12))
+    radio_buttons.append(radio_button)
+next_button = tk.Button(root, text="Next", command=check_answer, font=("Arial", 12), state="disabled")
 
-# Set the initial question label and answer buttons
-question_label.config(textvariable=question_var)
-for button in answer_buttons:
-    button.pack(pady=2)
+# Pack the widgets
+question_label.pack(pady=20)
+for radio_button in radio_buttons:
+    radio_button.pack(pady=5)
+next_button.pack(pady=20)
 
-# Run the main loop
+# Ask the first question
+ask_question()
+
+# Start the main loop
 root.mainloop()
